3fa29db506d0b13b9436d124413afb1b93a0adc1
sh-iv-am <shivam.shashank@juspay.in>
2025-08-16 08:24:00 +0530

    initial

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict
+ * @format
+ */
+
 class ReScriptParser {
   constructor(input, options = {}) {
     if (typeof input !== 'string') {
